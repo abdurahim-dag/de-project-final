3ad8a567d14359fc7dcf73d249d70d9333ca75e7
abdurahim <abdurahim.dag@gmail.com>
2023-05-24 15:39:33 +0300

    fix delete .idea

--- src/dags/staging/storage/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model import Workflow
-from .base import BaseStorage
-from .pg import WorkflowStorage
--- src/sql/stg_dst_vertica.sql
+++
@@ -1,2 +0,0 @@
-/*Батчовая загрузка в стейджинг.*/
-COPY ST23051601__STAGING.%(table_name)s(%(column_names)s) FROM LOCAL STDIN DELIMITER ',' ENCLOSED BY '"' REJECTED DATA AS TABLE ST23051601__STAGING.%(table_name)s_rej;
